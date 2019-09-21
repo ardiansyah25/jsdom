af84a57d3af83bb284cb313d05873ad2046531a1
ardiansyah <aardi25@gmail.com>
2019-09-22 05:28:12 +0700

    klk

---
+++ script.js
@@ -0,0 +1,16 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = 'Ardiansyah';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'Hello World';
+
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'Salmon';
+
+// const judul = document.getElementsByTagName('h1')[0];
+// const a = document.querySelector('section#a a');
+
+// const p2 = document.querySelector('.p2');
+// p2.classList.add('label');
